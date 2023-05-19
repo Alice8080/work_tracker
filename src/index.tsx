76363f39d5974f1ff1b262ced9a126e7f73a02b8
Alice <alisapyulzyu@gmail.com>
2023-05-19 03:05:26 +0300

    Update router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import store from './store';
 import App from './App';
 
